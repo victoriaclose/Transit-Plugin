5bf234734eccb86782acea733bd8a24c9c50cb23
Close <vclose85@engr-134-20.eastern.ewu.edu>
2013-11-04 13:45:14 -0800

    Added Plugins

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,19 @@
 
 $(document).ready(function(){
 	
-	// Good Stuff
+	$('body').hide('fast').show('slow');
+	
+	$('img').click(function(){
+		
+		// Move Image
+		$(this).transition({
+			perspective: '100px',
+  			rotateY: '180deg'
+		}, 1000, 'snap').transition({
+			x : '200px'
+			
+		});	
+		
+	});
 	
 });
